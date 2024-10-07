0754d8ccf3b6cc27a41064fe918f477c8485ec51
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2024-10-08 04:29:00 +0600

    Blogs page created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
     return (
-        <div className="hero">
+        <div className="hero -mt-32">
             <div className="hero-content text-center">
                 <div className="max-w-7xl">
                     <h1 className="text-5xl font-bold">Welcome to&nbsp;
@@ -23,7 +23,7 @@ const Hero = () => {
                         <Link to='/bookmarks' className="relative inline-block px-4 py-2 font-medium group">
                             <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-secondary group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
                             <span className="absolute inset-0 w-full h-full bg-white border-2 border-secondary group-hover:bg-secondary"></span>
-                            <span className="relative text-black group-hover:text-base-100">Read Blogs</span>
+                            <span className="relative text-black group-hover:text-base-100">Bookmarks</span>
                         </Link>
 
                     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import wave from "../assets/wave.svg"
 
 const Home = () => {
     return (
-        <div className="flex flex-col justify-center items-center min-h-[calc(100vh-116px)]">
+        <div className="flex relative flex-col justify-center items-center min-h-[calc(100vh-116px)]">
             <Hero />
+            <img className="w-full absolute bottom-0" src={wave} alt="" />
         </div>
     );
 };
